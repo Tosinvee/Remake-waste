d34e349a1fe3596c4056b313b180934e2e760fbe
Bayode <bayodeoluwatosin06@gmail.com>
2024-10-20 09:38:30 +0100

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
     forbidNonWhitelisted:true,
@@ -12,7 +13,8 @@ async function bootstrap() {
     transformOptions:{
       enableImplicitConversion:true
     }
-  }))
+  }));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
